94d5bc9ea291c84313986eec9d64438cdfe077c1
JosiRC <josi.reyescampos@gmail.com>
2023-08-07 23:47:05 -0600

    Add users info

---
+++ js/usuarios.js
@@ -0,0 +1,5 @@
+var cuentas = [
+    { nombre: "Mali", saldo: 200, password: "12345" },
+    { nombre: "Gera", saldo: 290, password: "contra1" },
+    { nombre: "Maui", saldo: 67, password: "andThanku" }
+];
