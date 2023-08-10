6db7b4ce59e2a3f6ba55c2f77ffa890fc08c60cb
JosiRC <josi.reyescampos@gmail.com>
2023-08-10 13:57:58 -0600

    login | Add login logic v1

--- js/login-logic.js
+++ js/login-logic.js
@@ -2,6 +2,23 @@ var userSigned = false;
 
 function checkUserSigned() {
     console.log("INICIA ==> checkUserSigned()")
+    alert("userSigned: "+userSigned)
     if (!userSigned)
-    window.location.href = 'login.html';
+        window.location.href = 'login.html';
 }
+
+document.querySelector('form')
+    .addEventListener('submit', e=> {
+        e.preventDefault()
+        const data = Object.fromEntries(
+            new FormData(e.target)
+        )
+        for (cuenta of cuentas) {
+            if(data.user == cuenta.nombre && data.password == cuenta.password) {
+                userSigned = true;
+                window.location.href = 'dashboard.html';
+            }
+        }
+        alert(JSON.stringify(data))
+        console.log(cuentas)
+    })
