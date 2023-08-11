b951caf052ed8eae004779dde7467bfb72cfc270
Josimar Reyes Campos <darth_jos@hotmail.com>
2023-08-10 20:39:33 -0600

    login | Add logoff and userData geter and seter

--- js/login-logic.js
+++ js/login-logic.js
@@ -1,24 +1,42 @@
 var userSigned = false;
 
+/* VALIDA SESION INICIADA */
 function checkUserSigned() {
-    console.log("INICIA ==> checkUserSigned()")
-    alert("userSigned: "+userSigned)
+    userSigned = sessionStorage.getItem(userSigned)
+
     if (!userSigned)
         window.location.href = 'login.html';
 }
 
+/* LOGIN  */
 document.querySelector('form')
     .addEventListener('submit', e=> {
         e.preventDefault()
         const data = Object.fromEntries(
             new FormData(e.target)
         )
+
         for (cuenta of cuentas) {
             if(data.user == cuenta.nombre && data.password == cuenta.password) {
-                userSigned = true;
-                window.location.href = 'dashboard.html';
+                sessionStorage.setItem(userSigned,true);
+                setUserData(cuenta);
+                window.location.href = 'index.html';
             }
         }
-        alert(JSON.stringify(data))
-        console.log(cuentas)
     })
+
+/* LOG OFF */
+function logOff(){
+    alert('Vuelva Pronto');
+    sessionStorage.clear();
+    window.location.href = 'login.html';
+}
+
+function setUserData(data) {
+    console.log("INICIA ==> setUserData()")
+    sessionStorage.setItem('userData',JSON.stringify(data));
+}
+
+function getUserData() {
+    return JSON.parse(sessionStorage.userData);
+}
